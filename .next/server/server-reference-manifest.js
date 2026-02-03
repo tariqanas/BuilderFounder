6330ce41142e08ed824b3df8e8da4a3aa2f7f7e8
Anas <tariq.tariq.anas@gmail.com>
2026-02-03 23:09:44 +0100

    fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gLOJs8+l1zjMip5t/qtYbrdspzCEA/QZbxFpxRjgwX8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
